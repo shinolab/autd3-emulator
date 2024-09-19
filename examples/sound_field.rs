2f0daafdceebdf564bac6e5492bfe88e83637538
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-09-19 13:44:12 +0900

    refactor `sound_pressure`

--- examples/sound_field.rs
+++ examples/sound_field.rs
@@ -4,21 +4,21 @@ use anyhow::Result;
 
 use autd3::prelude::*;
 use autd3_link_emulator::{
-    recording::{Range, RecordOption, TimeRange},
+    recording::{Range, RecordOption},
     Emulator,
 };
 use polars::{io::SerWriter, prelude::CsvWriter};
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    // let mut autd =
-    //     Controller::builder([AUTD3::new(Vector3::zeros()), AUTD3::new(Vector3::zeros())])
-    //         .open(Emulator::builder())
-    //         .await?;
+    let mut autd =
+        Controller::builder([AUTD3::new(Vector3::zeros()), AUTD3::new(Vector3::zeros())])
+            .open(Emulator::builder())
+            .await?;
 
-    // let focus = autd.geometry().center() + Vector3::new(0., 0., 150. * mm);
+    let focus = autd.geometry().center() + Vector3::new(0., 0., 150. * mm);
 
-    // // plot focus
+    // // plot sound field around focus
     // {
     //     autd.send(Silencer::disable()).await?;
     //     autd.start_recording()?;
@@ -35,46 +35,44 @@ async fn main() -> Result<()> {
     //             z: focus.z..=focus.z,
     //             resolution: 1.,
     //         },
+    //         Duration::ZERO..=Duration::from_millis(1),
     //         RecordOption {
-    //             time: Some(TimeRange {
-    //                 duration: Duration::ZERO..=Duration::from_millis(1),
-    //                 time_step_s: Duration::from_micros(1).as_secs_f32(),
-    //             }),
+    //             time_step: Duration::from_micros(1),
     //             print_progress: true,
     //             ..Default::default()
     //         },
     //     );
-    //     CsvWriter::new(std::fs::File::create("sound_field_focus.csv")?)
+    //     CsvWriter::new(std::fs::File::create("sound_field_around_focus.csv")?)
     //         .include_header(true)
     //         .finish(&mut df)?;
-    //     println!("Focus sound field data is saved as sound_field_focus.csv");
+    //     println!("Focus sound field data is saved as sound_field_around_focus.csv");
     // }
 
-    // // plot focus with 200Hz sin modulation
-    // {
-    //     autd.send(Silencer::default()).await?;
-    //     autd.start_recording()?;
-    //     autd.send((Sine::new(200. * Hz), Focus::new(focus))).await?;
-    //     autd.tick(Duration::from_millis(20))?;
-    //     let record = autd.finish_recording()?;
+    // plot focus under 200Hz sin modulation with silencer
+    {
+        autd.send(Silencer::default()).await?;
+        autd.start_recording()?;
+        autd.send((Sine::new(200. * Hz), Focus::new(focus))).await?;
+        autd.tick(Duration::from_millis(20))?;
+        let record = autd.finish_recording()?;
 
-    //     println!("Calculating sound field at focus...");
-    //     let mut df = record[0].sound_field_at(
-    //         &focus,
-    //         RecordOption {
-    //             time: Some(TimeRange {
-    //                 duration: Duration::ZERO..=Duration::from_millis(20),
-    //                 time_step_s: ULTRASOUND_PERIOD.as_secs_f32() / 256.,
-    //             }),
-    //             print_progress: true,
-    //             ..Default::default()
-    //         },
-    //     );
-    //     CsvWriter::new(std::fs::File::create("sound_field_focus_with_am.csv")?)
-    //         .include_header(true)
-    //         .finish(&mut df)?;
-    //     println!("Focus sound field data is saved as sound_field_focus_with_am.csv");
-    // }
+        println!("Calculating sound pressure at focus under 200Hz sin modulation with silencer...");
+        let mut df = record[0].sound_pressure(
+            &focus,
+            Duration::ZERO..=Duration::from_millis(20),
+            RecordOption {
+                time_step: Duration::from_micros(1),
+                print_progress: true,
+                ..Default::default()
+            },
+        )?;
+        CsvWriter::new(std::fs::File::create(
+            "sound_pressure_at_focus_with_am.csv",
+        )?)
+        .include_header(true)
+        .finish(&mut df)?;
+        println!("Focus sound field data is saved as sound_pressure_at_focus_with_am.csv");
+    }
 
     // // plot STM
     // {
@@ -117,9 +115,9 @@ async fn main() -> Result<()> {
     //     println!("STM sound field data is saved as sound_field_stm.csv");
     // }
 
-    // autd.close().await?;
+    autd.close().await?;
 
-    // println!("See plot_field.py for visualization.");
+    println!("See plot_field.py for visualization.");
 
     Ok(())
 }
