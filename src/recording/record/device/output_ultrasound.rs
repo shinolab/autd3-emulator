1a8fcb61a257b6fe2564e03e5e8b089ad9f40898
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-09-24 09:29:58 +0900

    call `hstack_mut` outside loop

--- src/recording/record/device/output_ultrasound.rs
+++ src/recording/record/device/output_ultrasound.rs
@@ -5,6 +5,7 @@ use polars::prelude::DataFrame;
 use crate::{error::EmulatorError, recording::record::transducer};
 
 use super::DeviceRecord;
+
 pub struct OutputUltrasound<'a> {
     pub(crate) inner: Vec<transducer::output_ultrasound::OutputUltrasound<'a>>,
 }
@@ -13,18 +14,20 @@ impl<'a> OutputUltrasound<'a> {
     pub fn next(&mut self, duration: Duration) -> Result<DataFrame, EmulatorError> {
         let mut df = self.inner[0].next(duration)?;
         df.rename("p[a.u.]", "p_0[a.u.]".into()).unwrap();
-        self.inner
+        let series = self
+            .inner
             .iter_mut()
             .enumerate()
             .skip(1)
-            .for_each(|(i, tr)| {
+            .map(|(i, tr)| {
                 let mut d = tr.next(duration).unwrap();
                 d.rename("p[a.u.]", format!("p_{}[a.u.]", i).into())
                     .unwrap();
                 let mut d = d.take_columns();
-                let v = d.pop().unwrap();
-                df.hstack_mut(&[v]).unwrap();
-            });
+                d.pop().unwrap()
+            })
+            .collect::<Vec<_>>();
+        df.hstack_mut(&series).unwrap();
         Ok(df)
     }
 }
--- src/recording/record/device/output_voltage.rs
+++ src/recording/record/device/output_voltage.rs
@@ -6,14 +6,19 @@ impl<'a> DeviceRecord<'a> {
     pub fn output_voltage(&self) -> DataFrame {
         let mut df = self[0].output_voltage();
         df.rename("voltage[V]", "voltage_0[V]".into()).unwrap();
-        self.iter().enumerate().skip(1).for_each(|(i, tr)| {
-            let mut d = tr.output_voltage();
-            d.rename("voltage[V]", format!("voltage_{}[V]", i).into())
-                .unwrap();
-            let mut d = d.take_columns();
-            let voltage = d.pop().unwrap();
-            df.hstack_mut(&[voltage]).unwrap();
-        });
+        let series = self
+            .iter()
+            .enumerate()
+            .skip(1)
+            .map(|(i, tr)| {
+                let mut d = tr.output_voltage();
+                d.rename("voltage[V]", format!("voltage_{}[V]", i).into())
+                    .unwrap();
+                let mut d = d.take_columns();
+                d.pop().unwrap()
+            })
+            .collect::<Vec<_>>();
+        df.hstack_mut(&series).unwrap();
         df
     }
 }
