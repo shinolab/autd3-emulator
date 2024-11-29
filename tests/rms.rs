a0a4334404e2bf3fb91b3e805787c1a9314dd5c1
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-11-29 10:34:45 +0900

    rm needless borrow

--- tests/rms.rs
+++ tests/rms.rs
@@ -110,7 +110,8 @@ async fn record_rms_resume(
         z: point.z,
         resolution: 1.,
     };
-    let expect = record
+
+    let mut rms = record
         .sound_field(
             range.clone(),
             RmsRecordOption {
@@ -119,26 +120,28 @@ async fn record_rms_resume(
                 ..Default::default()
             },
         )
-        .await?
-        .next(10 * ULTRASOUND_PERIOD)
         .await?;
-
-    let mut rms = record
-        .sound_field(
-            range,
-            RmsRecordOption {
-                #[cfg(feature = "gpu")]
-                gpu,
-                ..Default::default()
-            },
-        )
-        .await?;
-    let mut v1 = rms.next(5 * ULTRASOUND_PERIOD).await?;
-    let v2 = rms.next(5 * ULTRASOUND_PERIOD).await?;
-    let columns = v2.get_columns();
-    v1.hstack_mut(&columns).unwrap();
-
-    assert_eq!(expect, v1);
+    assert_eq!(
+        record
+            .sound_field(
+                range,
+                RmsRecordOption {
+                    #[cfg(feature = "gpu")]
+                    gpu,
+                    ..Default::default()
+                },
+            )
+            .await?
+            .next(10 * ULTRASOUND_PERIOD)
+            .await?,
+        polars::functions::concat_df_horizontal(
+            &[
+                rms.next(5 * ULTRASOUND_PERIOD).await?,
+                rms.next(5 * ULTRASOUND_PERIOD).await?,
+            ],
+            false,
+        )?
+    );
 
     Ok(())
 }
